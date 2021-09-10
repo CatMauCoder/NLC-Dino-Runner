70d8181506741dbe86acc1b14955e4101e741b4c
CatMauCoder <wmauchess@gmail.com>
2021-09-09 20:13:22 -0400

    CLASE 3: Dino
    Implementamos  el objeto dinosaurio
    NOTA: No me corre por que me dice que las clases que instancio son insubscriptables :"( no encontre como solucionarlo,

---
+++ nlc_dino_runner/componentes/dinosaur.py
@@ -0,0 +1,90 @@
+#Importanto clas Sprite
+import pygame
+from pygame.sprite import Sprite
+
+
+from nlc_dino_runner.utils.constants import RUNNING, DUCKING, JUMPING
+
+
+class Dinosaur(Sprite):
+    #Coordenadas
+    x_pos = 80
+    y_pos = 310
+    y_POS_DUCK = 340
+    JUMP_VEL = 8
+
+    def __init__(self):
+        self.image = RUNNING[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.x_pos
+        self.dino_rect.y = self.y_pos
+        self.step_index = 0
+        self.dino_run = True
+        self.dino_jump = False
+        self.dino_duck = False
+        self.jump_vel = self.JUMP_VEL
+
+
+
+    def update(self,user_input):
+        if self.dino_jump:
+            self.jump()
+        if self.dino_duck:
+            self.duck()
+        if self.dino_run:
+            self.run()
+
+
+        if user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_jump = False
+            self.dino_run = False
+
+        elif user_input[pygame.K_UP] and not self.dino_jump:
+
+            self.dino_duck = False
+            self.dino_jump = True
+            self.dino_run = False
+        elif not self.dino_jump:
+            self.dino_duck = True
+            self.dino_jump = False
+            self.dino_run = True
+
+
+
+        else:
+            self.run()
+
+        if self.stop_index >= 10:
+            self.step_index = 0
+
+    def run(self):
+        self.image = RUNNING[0] if self.stop_index <5 else RUNNING[1]
+
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.x_pos
+        self.dino_rect.y= self.y_pos
+        self.step_index += 1
+
+    def draw(self, screen):
+        #funcion blit para dibujar
+        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.x_pos
+        self.dino_rect.y = self.y_POS_DUCK
+        self.step_index +=1
+
+    def jump(self):
+        self.image = JUMPING
+        if self.dino_jump:
+            self.dino_rect.y -= self.jump_vel * 4
+            self.jump_vel -= 1
+
+        if self.jump_vel < -self.JUMP_VEL:
+            self.dino_rect.y = self.y_pos
+            self.dino_jump = False
+            self.jump_vel = self.JUMP_VEL
+
--- nlc_dino_runner/componentes/game.py
+++ nlc_dino_runner/componentes/game.py
@@ -1,19 +1,25 @@
 import pygame
 
+from nlc_dino_runner.componentes.dinosaur import Dinosaur
 from nlc_dino_runner.utils.constants import TITLE, ICON, SCREEN_WIDTH, SCREEN_HEIGHT, BG, FPS
 
 
 class Game:
     def __init__(self):
         pygame.init()
+        #Poniendo titulo al juego
         pygame.display.set_caption(TITLE)
+        #Poniendo Icono
         pygame.display.set_icon(ICON)
+        #Reloj de tiempo
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.playing = False
+        #VAriables de coordenadas
         self.x_pos_bg = 0
         self.y_pos_bg = 360
         self.game_speed = 20
+        self.player = Dinosaur()
 
     def run(self):
         self.playing = True
@@ -26,25 +32,29 @@ class Game:
 
     def event(self):
         for event in pygame.event.get():
+            #Detentanto si se salio del programa
             if event.type == pygame.QUIT:
                 self.playing = False
 
-
     def update(self):
-        print("Actualizando")
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
+        #Ciclos por segundo
         self.clock.tick(FPS)
+        #Dibujamos el color
         self.screen.fill((255, 255, 255))
+        #Agregando Fondo
         self.draw_background()
-
-
+        #dibujando dino
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
     def draw_background(self):
         image_width = BG.get_width()
-        # aGREGANDO IMAGEN A SCREEN
+        # AGREGANDO IMAGEN A SCREEN
         self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
         self.screen.blit(BG, (self.x_pos_bg + image_width, self.y_pos_bg))
         if self.x_pos_bg <= -image_width:
